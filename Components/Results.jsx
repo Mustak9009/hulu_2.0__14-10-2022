51b8a7cedb664c7a6b168d72be8249af03200c2f
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-10-18 06:14:51 +0530

    Solve Result file error -> FlipMove

--- Components/Results.jsx
+++ Components/Results.jsx
@@ -4,12 +4,12 @@ import FlipMove from "react-flip-move";
 
 export default function Results({ results }) {
   return (
-    <>
+    <div>
       <FlipMove className="mx-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 3xl:flex justify-center flex-wrap">
         {results.map((result) => (
           <Thumbnail result={result} key={result.id} />
         ))}
       </FlipMove>
-    </>
+    </div>
   );
 }
